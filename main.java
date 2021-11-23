ce39729c563e013fc3c78221f2d9730933b1fd06
TanShaRu <nathan2506@hotmail.fr>
2021-11-23 19:34:54 +0100

    test

---
+++ main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+       
+        System.out.print("Veuillez saisir une valeur a et b : ");
+       
+        int a = Console.lireInt();            
+        int b = Console.lireInt();
+
+        int c = a*b;
+
+        System.out.print ("le resultat est : "+c);
+    }
+}
